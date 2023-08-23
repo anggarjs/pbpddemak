15354816b764b3df5dbe9693f467fb1975874a0d
anggarjs <rajasa.angga@gmail.com>
2023-08-23 16:32:40 +0700

    ubah view form

--- application/views/form_tambah_user.php
+++ application/views/form_tambah_user.php
@@ -5,25 +5,7 @@
         <!-- Bread crumb and right sidebar toggle -->
         <!-- ============================================================== -->
         <div class="page-breadcrumb">
-          <div class="row">
-            <div class="col-5 align-self-center">
-              <h4 class="page-title">Form Tambah User</h4>
-            </div>
-            <div class="col-7 align-self-center">
-              <div class="d-flex align-items-center justify-content-end">
-                <nav aria-label="breadcrumb">
-                  <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                      <a href="#">Home</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">
-                      Form Basic
-                    </li>
-                  </ol>
-                </nav>
-              </div>
-            </div>
-          </div>
+
         </div>
         <!-- ============================================================== -->
         <!-- End Bread crumb and right sidebar toggle -->
@@ -38,6 +20,11 @@
           <div class="row">
             <div class="col-12">
               <div class="card">
+				<div class="card-header bg-info">
+					<h4 class="card-title text-white">
+					Form Penambahan Data User
+					</h4>
+				</div>
                 <form class="form-horizontal">
                   <div class="card-body">
                     <div class="row">
--- application/views/form_upload_rab.php
+++ application/views/form_upload_rab.php
@@ -5,25 +5,7 @@
         <!-- Bread crumb and right sidebar toggle -->
         <!-- ============================================================== -->
         <div class="page-breadcrumb">
-          <div class="row">
-            <div class="col-5 align-self-center">
-              <h4 class="page-title">Upload File RAB</h4>
-            </div>
-            <div class="col-7 align-self-center">
-              <div class="d-flex align-items-center justify-content-end">
-                <nav aria-label="breadcrumb">
-                  <ol class="breadcrumb">
-                    <li class="breadcrumb-item">
-                      <a href="#">Home</a>
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">
-                      Form Basic
-                    </li>
-                  </ol>
-                </nav>
-              </div>
-            </div>
-          </div>
+
         </div>
         <!-- ============================================================== -->
         <!-- End Bread crumb and right sidebar toggle -->
@@ -41,30 +23,37 @@
               <!-- ----------------------------------------- -->
               <!-- 3. Custom File Upload -->
               <!-- ----------------------------------------- -->
-              <div class="card card-body">
-                <div class="mb-3">
-				  <form>
-					<input class="form-control" type="file" id="formFile" />
-					  <hr />
+              <div class="card">
+					<div class="card-header bg-info">
+						<h4 class="card-title text-white">
+						Form Upload RAB
+						</h4>
+					</div>
+				  <div class="card card-body">
+					<div class="mb-3">
+					  <form>
+						<input class="form-control" type="file" id="formFile" />
+						  <hr />
 
-					  <div class="p-3 border-top">
-						<div class="text-end">
-						  <button
-							type="submit"
-							class="
-							  btn btn-info
-							  rounded-pill
-							  px-4
-							  waves-effect waves-light
-							"
-						  >
-							Save
-						  </button>
-						</div>
-					  </div>
-				  </form>
-                </div>
-              </div>
+						  <div class="p-3 border-top">
+							<div class="text-end">
+							  <button
+								type="submit"
+								class="
+								  btn btn-info
+								  rounded-pill
+								  px-4
+								  waves-effect waves-light
+								"
+							  >
+								Save
+							  </button>
+							</div>
+						  </div>
+					  </form>
+					</div>
+				  </div>
+			  </div>
             </div>
           </div>
           <!-- Row -->
