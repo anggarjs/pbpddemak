6450141275751f50e233522ff26794f41cf42abe
rizkidwimartanto <rizkidwimartanto@gmail.com>
2023-08-28 15:00:29 +0700

    menambahkan fitur tambah capel

--- application/views/Capel/form_tambah_capel.php
+++ application/views/Capel/form_tambah_capel.php
@@ -1,103 +1,107 @@
       <!-- Page wrapper  -->
       <!-- ============================================================== -->
-	<div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <div class="page-breadcrumb">
+      <div class="page-wrapper">
+      	<!-- ============================================================== -->
+      	<!-- Bread crumb and right sidebar toggle -->
+      	<!-- ============================================================== -->
+      	<div class="page-breadcrumb">
 
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
-        <div class="container-fluid">
-          <!-- -------------------------------------------------------------- -->
-          <!-- Start Page Content -->
-          <!-- -------------------------------------------------------------- -->
-			<div class="row">
-				<div class="col-12">
-					<div class="card">
-						<div class="card-header bg-info">
-							<h4 class="card-title text-white">
-							Form Penambahan Data User
-							</h4>
-						</div>
-						
-						<?php 
-						$attributes 	= array('class' => 'was-validated');
-						echo form_open('User/Tambah',$attributes);
-						?>
-						<div class="card-body">
+      	</div>
+      	<!-- ============================================================== -->
+      	<!-- End Bread crumb and right sidebar toggle -->
+      	<!-- ============================================================== -->
+      	<!-- ============================================================== -->
+      	<!-- Container fluid  -->
+      	<!-- ============================================================== -->
+      	<div class="container-fluid">
+      		<!-- -------------------------------------------------------------- -->
+      		<!-- Start Page Content -->
+      		<!-- -------------------------------------------------------------- -->
+      		<div class="row">
+      			<div class="col-12">
+      				<div class="card">
+      					<div class="card-header bg-info">
+      						<h4 class="card-title text-white">
+      							Form Penambahan Data Capel
+      						</h4>
+      					</div>
+      					<div class="card-body">
 							<div class="mb-3">
-								<label for="validationTextarea">Username</label>
-								<input
-								type="text"
-								name="username"
-								class="form-control"
-								required
-								/>
+								<form action="<?php echo base_url('index.php/Capel/Tambah');?>" method="post">
+      							<label>Nama Capel</label>
+      							<input type="text" name="nama_capel" id="nama_capel" class="form-control <?php echo (form_error('nama_capel')) ? 'is-invalid' : ''; ?>" />
+      							<div class="invalid-feedback">
+									<?php if(form_error('nama_capel') == true) : ?>
+										<?= form_error('nama_capel'); ?>
+									<?php endif; ?>
+								</div>
+      						</div>
+      						<div class="mb-3">
+      							<label>Daya Capel</label>
+      							<input type="number" name="daya_capel" id="daya_capel" class="form-control <?php echo (form_error('daya_capel')) ? 'is-invalid' : ''; ?>" />
+								<div class="invalid-feedback">
+									<?php if(form_error('daya_capel') == true) : ?>
+										<?= form_error('daya_capel'); ?>
+									<?php endif; ?>
+								</div>
 							</div>
-
-							<div class="form-group mb-3">
-								<label for="validationTextarea">Asal Unit Kerja</label>
-								<?php
-									if(set_value('pilihan_ulp')!='') $set_select = set_value('pilihan_ulp');
-									else $set_select = '';					
-									echo form_dropdown('pilihan_ulp',$set_select,'class="form-select" required');
+      						<div class="mb-3">
+      							<label>Biaya Penyambungan</label>
+      							<input type="number" name="biaya_penyambungan" id="biaya_penyambungan" class="form-control <?php echo (form_error('biaya_penyambungan')) ? 'is-invalid' : ''; ?>" />
+								<div class="invalid-feedback">
+									<?php if(form_error('biaya_penyambungan') == true) : ?>
+										<?= form_error('biaya_penyambungan'); ?>
+									<?php endif; ?>
+								</div>
+							</div>
+      						<div class="mb-3">
+      							<label>Biaya Investasi</label>
+      							<input type="number" name="biaya_investasi" id="biaya_investasi" class="form-control <?php echo (form_error('biaya_investasi')) ? 'is-invalid' : ''; ?>" />
+								<div class="invalid-feedback">
+									<?php if(form_error('biaya_investasi') == true) : ?>
+										<?= form_error('biaya_investasi'); ?>
+									<?php endif; ?>
+								</div>
+							</div>
+      						<div class="mb-3">
+      							<label>Nama ULP</label>
+      							<?php
+								if (set_value('pilihan_ulp') != '') $set_select = set_value('pilihan_ulp');
+								else $set_select = '';
+								echo form_dropdown('pilihan_ulp', $pilihan_ulp, $set_select, 'class="form-select"');
 								?>
-								<div class="invalid-feedback">
-								Pilih Unit Kerja
-								</div>
-							</div>
-							
-							<div class="form-group mb-3">
-								<label for="validationTextarea">Role Kerja</label>
-								<?php
-									if(set_value('pilihan_role')!='') $set_select = set_value('pilihan_role');
-									else $set_select = '';					
-									echo form_dropdown('pilihan_role',$set_select,'class="form-select" required ');
-								?>					  
-								<div class="invalid-feedback">
-								Pilih Role User
-								</div>
-							</div>						  
-						</div>
-						<div class="p-3 border-top">
-							<div class="text-end">
-								<button
-								type="submit"
-								class="
+      						</div>
+      					</div>
+      					<div class="p-3 border-top">
+      						<div class="text-end">
+      							<button type="submit" class="
 								btn btn-info
 								rounded-pill
 								px-4
 								waves-effect waves-light
-								"
-								>
-								Save
-								</button>
-							</div>
-						</div>
-					<!-- </form> -->
-					<?php echo form_close(); ?>
-					</div>
-				</div><!-- end <div class="col-12"> -->
-			</div>
-          <!-- End Row -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Container fluid  -->
-        <!-- ============================================================== -->
-		
-        <!-- ============================================================== -->
-        <!-- footer -->
-        <!-- ============================================================== -->
-        <footer class="footer text-center">
-         
-        </footer>
-        <!-- ============================================================== -->
-        <!-- End footer -->
-        <!-- ============================================================== -->
-    </div>
+								">
+      								Save
+      							</button>
+      						</div>
+      					</div>
+      					<!-- </form> -->
+						</form>
+      				</div>
+      			</div><!-- end <div class="col-12"> -->
+      		</div>
+      		<!-- End Row -->
+      	</div>
+      	<!-- ============================================================== -->
+      	<!-- End Container fluid  -->
+      	<!-- ============================================================== -->
+
+      	<!-- ============================================================== -->
+      	<!-- footer -->
+      	<!-- ============================================================== -->
+      	<footer class="footer text-center">
+
+      	</footer>
+      	<!-- ============================================================== -->
+      	<!-- End footer -->
+      	<!-- ============================================================== -->
+      </div>
--- application/views/User/view_all_user.php
+++ application/views/User/view_all_user.php
@@ -1,119 +1,103 @@
       <!-- Page wrapper  -->
       <!-- ============================================================== -->
-	<div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <div class="page-breadcrumb">
+      <div class="page-wrapper">
+      	<!-- ============================================================== -->
+      	<!-- Bread crumb and right sidebar toggle -->
+      	<!-- ============================================================== -->
+      	<div class="page-breadcrumb">
 
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
-        <div class="container-fluid">
-          <!-- -------------------------------------------------------------- -->
-          <!-- Start Page Content -->
-          <!-- -------------------------------------------------------------- -->
-			<div class="row">
-				<div class="col-12">
-					<div class="card">
-						<div class="card-header bg-info">
-							<h4 class="card-title text-white">
-							Data User
-							</h4>
-						</div>
-						<div class="card-body">
-							<h6 class="card-subtitle mb-3">
-							<div class="table-responsive mt-3">
-								<table
-								id="sing_row_del"
-								class="
+      	</div>
+      	<!-- ============================================================== -->
+      	<!-- End Bread crumb and right sidebar toggle -->
+      	<!-- ============================================================== -->
+      	<!-- ============================================================== -->
+      	<!-- Container fluid  -->
+      	<!-- ============================================================== -->
+      	<div class="container-fluid">
+      		<!-- -------------------------------------------------------------- -->
+      		<!-- Start Page Content -->
+      		<!-- -------------------------------------------------------------- -->
+      		<div class="row">
+      			<div class="col-12">
+      				<div class="card">
+      					<div class="card-header bg-info">
+      						<h4 class="card-title text-white">
+      							Data User
+      						</h4>
+      					</div>
+      					<div class="card-body">
+      						<h6 class="card-subtitle mb-3">
+      							<div class="table-responsive mt-3">
+      								<table id="sing_row_del" class="
 								table table-striped table-bordered
 								display
 								text-nowrap
-								"
-								style="width: 100%"
-								>
-								<thead>
-									<!-- start row -->
-									<tr>
-									<th>Action</th>
-									<th>Nama User</th>
-									<th>Asal Unit Kerja</th>
-									<th>Role User</th>
-									
-									</tr>
-									<!-- end row -->
-								</thead>
-								<tbody>
-									<?php
-									foreach($data_users->result() as $row){
-										echo '<tr>';
-									?>
-                    <td>
-                      <div class="dropdown dropstart">
-                        <a
-                          href="#"
-                          class="link"
-                          id="dropdownMenuButton"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false"
-                        >
-                          <i
-                            data-feather="more-horizontal"
-                            class="feather-sm"
-                          ></i>
-                        </a>
-                        <ul
-                          class="dropdown-menu"
-                          aria-labelledby="dropdownMenuButton"
-                        >
-                          <li><a class="dropdown-item" href="#">Edit</a></li>
-                          <li><a class="dropdown-item" href="#">Delete</a></li>
-                        </ul>
-                      </div>
-                    </td>									
-									<?php
-										echo '<td>'.$row->nama_user.'</td>';
-										echo '<td>'.$row->nama_ulp.'</td>';
-										echo '<td>'.$row->nama_role.'</td>';
-										echo '</tr>';
-									}
-									?>
-									<!-- end row -->
-								</tbody>
-								<tfoot>
-									<!-- start row -->
-									<tr>
-									<th>Action</th>
-									<th>Nama User</th>
-									<th>Asal Unit Kerja</th>
-									<th>Role User</th>
-									</tr>
-									<!-- end row -->
-								</tfoot>
-							</table>
-							</div>
-						</div>
-					</div>
-				</div><!-- end <div class="col-12"> -->
-			</div>
-          <!-- End Row -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Container fluid  -->
-        <!-- ============================================================== -->
-		
-        <!-- ============================================================== -->
-        <!-- footer -->
-        <!-- ============================================================== -->
-        <footer class="footer text-center">
-         
-        </footer>
-        <!-- ============================================================== -->
-        <!-- End footer -->
-        <!-- ============================================================== -->
-    </div>
+								" style="width: 100%">
+      									<thead>
+      										<!-- start row -->
+      										<tr>
+      											<th>Action</th>
+      											<th>Nama User</th>
+      											<th>Asal Unit Kerja</th>
+      											<th>Role User</th>
+
+      										</tr>
+      										<!-- end row -->
+      									</thead>
+      									<tbody>
+      										<?php
+											foreach ($data_users->result() as $row) {
+												echo '<tr>';
+											?>
+      											<td>
+      												<div class="dropdown dropstart">
+      													<a href="#" class="link" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+      														<i data-feather="more-horizontal" class="feather-sm"></i>
+      													</a>
+      													<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      														<li><a class="dropdown-item" href="#">Edit</a></li>
+      														<li><a class="dropdown-item" href="#">Delete</a></li>
+      													</ul>
+      												</div>
+      											</td>
+      										<?php
+												echo '<td>' . $row->nama_user . '</td>';
+												echo '<td>' . $row->nama_ulp . '</td>';
+												echo '<td>' . $row->nama_role . '</td>';
+												echo '</tr>';
+											}
+											?>
+      										<!-- end row -->
+      									</tbody>
+      									<tfoot>
+      										<!-- start row -->
+      										<tr>
+      											<th>Action</th>
+      											<th>Nama User</th>
+      											<th>Asal Unit Kerja</th>
+      											<th>Role User</th>
+      										</tr>
+      										<!-- end row -->
+      									</tfoot>
+      								</table>
+      							</div>
+      					</div>
+      				</div>
+      			</div><!-- end <div class="col-12"> -->
+      		</div>
+      		<!-- End Row -->
+      	</div>
+      	<!-- ============================================================== -->
+      	<!-- End Container fluid  -->
+      	<!-- ============================================================== -->
+
+      	<!-- ============================================================== -->
+      	<!-- footer -->
+      	<!-- ============================================================== -->
+      	<footer class="footer text-center">
+
+      	</footer>
+      	<!-- ============================================================== -->
+      	<!-- End footer -->
+      	<!-- ============================================================== -->
+      </div>
