aff40e034b15018b040e18cae743b3bbfa8c2cc9
anggarjs <rajasa.angga@gmail.com>
2023-08-28 11:59:23 +0700

    perbaikan validasi form sukses

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -32,7 +32,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     />
     <!-- Custom CSS -->
     <link href="<?php echo base_url()?>dist/css/style.min.css" rel="stylesheet" />
-	<link href="<?php echo base_url()?>dist/css/error.css" rel="stylesheet" />
+	<link href="<?php echo base_url()?>css/error.css" rel="stylesheet" />
     <!-- This page plugin CSS -->
     <link rel="stylesheet" href="<?php echo base_url()?>dist/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css">	
 
@@ -392,7 +392,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!--This page plugins -->
     <script src="<?php echo base_url()?>dist/libs/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="<?php echo base_url()?>dist/js/pages/datatable/custom-datatable.js"></script>
-    <script src="<?php echo base_url()?>dist/js/pages/datatable/datatable-basic.init.js"></script>	
+    <script src="<?php echo base_url()?>dist/js/pages/datatable/datatable-basic.init.js"></script>
+	<script src="<?php echo base_url()?>dist/js/pages/datatable/datatable-api.init.js"></script>
+ 
+	
+    <!-- start - This is for export functionality only -->
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
+    <script src="<?php echo base_url()?>dist/js/pages/datatable/datatable-advanced.init.js"></script>	
   </body>
 </html>
 
