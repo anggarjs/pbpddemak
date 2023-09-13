9eaa4471fba957fdf2a1aa09f6d57a61b467a10b
rizkidwimartanto <rizkidwimartanto@gmail.com>
2023-09-13 14:12:49 +0700

    memindahkan script checklist ke file dan fitur hapus berhasil ketika memindahkan pagination

--- application/views/User/view_all_user.php
+++ application/views/User/view_all_user.php
@@ -125,3 +125,53 @@
       	<!-- End footer -->
       	<!-- ============================================================== -->
       </div>
+      <script>
+      	// Membuat checklist
+      	document.addEventListener("DOMContentLoaded", function() {
+      		// Daftar elemen checkbox "checklist all" dan checkbox data
+      		var checkboxGroups = [{
+      			checklistAll: document.getElementById("checklist-user"),
+      			checkboxes: document.querySelectorAll('input[name="check[]"]')
+      		}, ];
+      		// end checklist
+
+      		// Tambahkan event listener untuk setiap grup checkbox
+      		checkboxGroups.forEach(function(group) {
+      			group.checklistAll.addEventListener("change", function() {
+      				// Set status checkbox data berdasarkan status checkbox "checklist all"
+      				group.checkboxes.forEach(function(checkbox) {
+      					checkbox.checked = group.checklistAll.checked;
+      				});
+      			});
+      		});
+      	});
+
+      	// sweetalert hapus
+      	document.addEventListener('DOMContentLoaded', function() {
+      		// Daftar tombol hapus
+      		var deleteButtons = document.querySelectorAll('.hapus-data-user');
+
+      		// Tambahkan event listener untuk setiap tombol hapus
+      		deleteButtons.forEach(function(button) {
+      			button.addEventListener('click', function(event) {
+      				event.preventDefault(); // Mencegah aksi default dari tombol hapus
+
+      				// Tampilkan SweetAlert konfirmasi
+      				Swal.fire({
+      					title: 'Konfirmasi',
+      					text: 'Apakah Anda yakin ingin menghapus data ini?',
+      					icon: 'warning',
+      					showCancelButton: true,
+      					confirmButtonText: 'Hapus',
+      					cancelButtonText: 'Batal'
+      				}).then((result) => {
+      					if (result.isConfirmed) {
+      						// Arahkan ke URL penghapusan data
+      						button.closest('form').submit();
+      					}
+      				});
+      			});
+      		});
+      	});
+      	// end sweetalert hapus
+      </script>
