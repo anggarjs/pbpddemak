8b34b165af01426dfff90b3f84bae92821ac9fb4
rizkidwimartanto <rizkidwimartanto@gmail.com>
2023-09-15 07:08:14 +0700

    testing commit

--- application/models/material_model.php
+++ application/models/material_model.php
@@ -59,12 +59,7 @@ class Material_model extends CI_Model
 		return $this->db->get('view_material_kurang')->result();
 	}
 	public function detail_material_kurang($id_detail_mdu){
-		$this->db->select("*");
-		$this->db->from("view_rincian_plgn_kurang_mdu");
-		$this->db->where("id_detail_mdu", $id_detail_mdu);
-		$query = $this->db->get()->result();
-		return $query;
-		// $this->db->where('nama_detail_mdu', $nama_detail_mdu);
-		// return $this->db->get('view_rincian_plgn_kurang_mdu')->result();
+		$this->db->where('id_detail_mdu', $id_detail_mdu);
+		return $this->db->get('view_rincian_plgn_kurang_mdu')->result();
 	}
 }//end of class
