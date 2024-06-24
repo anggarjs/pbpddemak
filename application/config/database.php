aa4e75ac3a0842b7d55cd9f46cc5ec1e346cc64f
ANGGA RAJASA <angga.rajasa@pusat.corp.pln.co.id>
2024-06-24 15:11:27 +0700

    update file

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '10.4.60.72',
+	'hostname' => '10.4.140.17',
 	'database' => 'pbpddemak',
 
 	/* 	'hostname' => 'db4free.net',
