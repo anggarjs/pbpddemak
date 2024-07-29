93ce09669c1cc5a9d18c78fddd5f5b111f0f2541
anggarjs <rajasa.angga@gmail.com>
2024-07-29 10:00:17 +0700

    a

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Test extends CI_Controller {
 	function index(){
 		if(isset($_SESSION['username']))
-			redirect('Input/upload_rab');
+			redirect('Test/Test_email');
 		else
 			redirect('Welcome');		
 	}
